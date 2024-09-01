ae6bd8ef7155e6fb57c93bc5b7b29f1884840992
neverhoodanx <neverhooda@yandex.ru>
2024-09-01 18:22:35 +0700

    add src

---
+++ lib.cpp
@@ -0,0 +1,16 @@
+/**
+ *  OTUS homework module
+ *  (c) 2024
+ */
+
+#include "lib.h"
+
+#include "version.h"
+
+namespace otus_cpp {
+
+int version() {
+	return PROJECT_VERSION_PATCH;
+}
+
+} // namespace otus_cpp
---
+++ main.cpp
@@ -0,0 +1,15 @@
+/**
+ *  OTUS homework module
+ *  manual test
+ *  (c) 2024
+ */
+
+#include "lib.h"
+
+#include <iostream>
+
+int main(int, char **) {
+	std::cout << "Version: " << otus_cpp::version() << std::endl;
+	std::cout << "Hello, world!" << std::endl;
+	return 0;
+}

a44132b407e69899d279f66b8420ed411dfc84ff
neverhoodanx <neverhooda@yandex.ru>
2024-12-11 19:16:47 +0700

    hw07 add a simple implementation of a terminal and tests for it

--- main.cpp
+++ main.cpp
@@ -4,19 +4,30 @@
  *  (c) 2024
  */
 
-#include "lib.hpp"
+#include "hw07_terminal/terminal.hpp"
+// #include "lib.hpp"
 
 #include <iostream>
 
-int main(int, char **) {
+int main(int argc, char *argv[]) {
+	// #7 HomeWork: terminal
+	if (argc != 2) {
+		std::cerr << "Usage: " << argv[0] << " <block_size>" << std::endl;
+		return 1;
+	}
+
+	size_t block_size = std::stoi(argv[1]);
+	otus_cpp::terminal terminal_(block_size, std::cin, std::cout);
+	terminal_.run();
+	return 0;
 
 	// #6 HomeWork: N Matrix
-	try {
-		otus_cpp::technical_task_matrix();
-	} catch (const std::exception &e) {
-		std::cerr << e.what() << std::endl;
-		return -1;
-	}
+	// try {
+	// 	otus_cpp::technical_task_matrix();
+	// } catch (const std::exception &e) {
+	// 	std::cerr << e.what() << std::endl;
+	// 	return -1;
+	// }
 
 	// #5 HomeWork: Graphic Editor
 	// try {
---
+++ src/hw07_terminal/command_handler.hpp
@@ -0,0 +1,39 @@
+/**
+ *  OTUS homework module
+ *  #7 terminal
+ *  (c) 2024
+ */
+#pragma once
+
+#include <ctime>
+#include <fstream>
+#include <iostream>
+#include <queue>
+#include <string>
+
+namespace otus_cpp {
+/**
+ * @class CommandHandler
+ * @class command_handler
+ * @brief Responsible for processing and logging command blocks.
+ */
+class command_handler {
+  public:
+	/**
+	 * @brief Constructs a CommandHandler with the specified output stream.
+	 * @param output_stream The output stream to which command blocks will be
+	 * written.
+	 */
+	command_handler(std::ostream &output_stream);
+
+	/**
+	 * @brief Processes a block of commands, logs them to console and a file.
+	 * @param commands Queue of commands to process.
+	 * @param time The initial time point
+	 */
+	void process_block(std::queue<std::string> commands, const std::string &time);
+
+  private:
+	std::ostream &out_; ///< Output stream for logging commands
+};
+} // namespace otus_cpp
